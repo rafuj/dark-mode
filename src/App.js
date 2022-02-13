9d7b1a33de93fd06ce9ac4a7d5cc9949c49b24bf
rafuj <raihanrafuj9978@gmail.com>
2022-02-14 00:26:14 +0600

    Complete Dark Mode App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Article from "./Article";
 import data from "./data";
-
+const getStorage = () => {
+	let theme = "light-theme";
+	if (localStorage.getItem("theme")) {
+		theme = localStorage.getItem("theme");
+	}
+	return theme;
+};
 function App() {
+	const [theme, setTheme] = useState(getStorage());
+
+	const toggleFunction = () => {
+		if (theme === "light-theme") {
+			setTheme("dark-theme");
+		} else {
+			setTheme("light-theme");
+		}
+	};
+
+	useEffect(() => {
+		document.documentElement.className = theme;
+		localStorage.setItem("theme", theme);
+	}, [theme]);
+
 	return (
 		<main>
 			<nav>
 				<div className="nav-center">
 					<h1>overreacted</h1>
-					<button className="btn">Toggle</button>
+					<button className="btn" onClick={toggleFunction}>
+						Toggle
+					</button>
 				</div>
 			</nav>
 			<section className="articles">
